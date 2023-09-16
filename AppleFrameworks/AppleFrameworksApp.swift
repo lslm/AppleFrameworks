0f183dcfd3fefdf299a9af02bec716da89e3b2c7
Lucas S <lucas.santos.silva2011@gmail.com>
2023-09-16 19:19:36 -0300

    Project bootstrap

---
+++ AppleFrameworks/AppleFrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksApp.swift
+//  AppleFrameworks
+//
+//  Created by Lucas Santos on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
