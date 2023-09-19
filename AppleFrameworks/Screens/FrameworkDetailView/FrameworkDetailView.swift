d79c0e9e8ad14be7bd0e4d41329d212ab556af06
Lucas S <lucas.santos.silva2011@gmail.com>
2023-09-19 00:55:06 -0300

    Support NavigationStack

--- AppleFrameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
+++ AppleFrameworks/Screens/FrameworkDetailView/FrameworkDetailView.swift
@@ -8,62 +8,51 @@
 import SwiftUI
 
 struct FrameworkDetailView: View {
-    @Environment(\.dismiss) var dismiss
     @State private var isShowingSafariView = false
     let framework: Framework
     
     var body: some View {
-        NavigationView {
-            ZStack {
-                ScrollView {
-                    VStack {
-                        Spacer()
-                        Image(framework.imageName)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(height: 120)
-                        
-                        Text(framework.name)
-                            .font(.title2)
-                            .bold()
-                            .padding(.top, 16)
-                        
-                        Text(framework.description)
-                            .font(.body)
-                            .padding(.top, 40)
-                            .foregroundColor(.secondary)
-                    }
-                    .toolbar {
-                        Button {
-                            dismiss()
-                        } label: {
-                            Text("Done")
-                                .foregroundStyle(Color.primary)
-                        }
-                    }
-                    .padding()
-                }
-                .safeAreaPadding(.bottom, 80)
-                
+        ZStack {
+            ScrollView {
                 VStack {
                     Spacer()
+                    Image(framework.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 120)
                     
-                    ZStack {
-                        Rectangle()
-                            .fill(.ultraThinMaterial)
-                            .ignoresSafeArea()
-                            .frame(maxHeight: 80)
-                            
-                        Button {
-                            isShowingSafariView = true
-                        } label: {
-                            PrimaryButtonView(text: "Learn More")
-                        }
+                    Text(framework.name)
+                        .font(.title2)
+                        .bold()
+                        .padding(.top, 16)
+                    
+                    Text(framework.description)
+                        .font(.body)
+                        .padding(.top, 40)
+                        .foregroundColor(.secondary)
+                }
+                .padding()
+            }
+            .safeAreaPadding(.bottom, 80)
+            
+            VStack {
+                Spacer()
+                
+                ZStack {
+                    Rectangle()
+                        .fill(.ultraThinMaterial)
+                        .ignoresSafeArea()
+                        .frame(maxHeight: 80)
+                    
+                    Button {
+                        isShowingSafariView = true
+                    } label: {
+                        PrimaryButtonView(text: "Learn More")
                     }
-                    .fullScreenCover(isPresented: $isShowingSafariView) {
-                        SafariView(url: URL(string: framework.urlString)!)
-                            .ignoresSafeArea()
-                    }
+                }
+                .fullScreenCover(isPresented: $isShowingSafariView) {
+                    SafariView(url: URL(string: framework.urlString)!)
+                        .ignoresSafeArea()
                 }
             }
         }
